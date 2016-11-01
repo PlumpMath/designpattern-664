844b60b1e4cb3170f6bbb24813eee2573a09472f
pengnix <superpengnix@gmail.com>
2016-11-01 19:13:40 +0800

    test2

--- GOF/src/com/pengnix/gof/helloword/MainClass.java
+++ GOF/src/com/pengnix/gof/helloword/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("first demo3");
+			System.out.println("first demo4");
 	}
 
 }
